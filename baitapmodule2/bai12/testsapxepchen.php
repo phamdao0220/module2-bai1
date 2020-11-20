4b7ff670680883aa0836ba54d69e14e122f79960
phamdao <phamd1411200220@gmail.com>
2020-11-20 14:29:05 +0700

    naitap

---
+++ baitapmodule2/bai12/testsapxepchen.php
@@ -0,0 +1,18 @@
+<?php
+function insertion_Sort($my_array){
+    for ($i=0;$i<count($my_array);$i++){
+        $val=$my_array[$i];
+        $j=$i-1;
+        while ($j>=0&&$my_array[$j]>$val){
+            $my_array[$j+1]=$my_array[$j];
+            $j--;
+        }
+        $my_array[$j+1]=$val;
+    }
+    return $my_array;
+}
+$test_array=array(3,0,2,5,-1,4,1);
+echo "Original Array"."<br>";
+echo implode(',',$test_array)."<br>";
+echo "Sorted Array:"."<br>";
+print_r(insertion_Sort($test_array));
